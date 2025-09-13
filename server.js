e2c201a52a94d7d513e8e18a689fb188be78c231
valentincorrea <vcyms.1209@icloud.com>
2025-09-12 21:48:18 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -5,30 +5,38 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
+// Import budget-data.json
+const budget = require("./budget-data.json");
+
 app.use(cors());
 
-const budget = {
-  myBudget: [
-    {
-      title: "Eat out",
-      budget: 25,
-    },
-    {
-      title: "Rent",
-      budget: 275,
-    },
-    {
-      title: "Grocery",
-      budget: 110,
-    },
-  ],
-};
+// const budget = {
+//   myBudget: [
+//     {
+//       title: "Eat out",
+//       budget: 25,
+//     },
+//     {
+//       title: "Rent",
+//       budget: 275,
+//     },
+//     {
+//       title: "Grocery",
+//       budget: 110,
+//     },
+//   ],
+// };
 
 app.use("/", express.static("public"));
 
+// Passing budget data to the frontend
 app.get("/budget", (req, res) => {
   res.json(budget);
 });
+
+// app.get("/budget", (req, res) => {
+//   res.json(budget);
+// });
 
 app.listen(port, () => {
   console.log(`API served at http://localhost:${port}`);
